7c4dc461927edf0110c8e2409258a0d20edbd18a
Ryan Cole <rcole@ilstech.com>
2013-06-28 14:07:16 -0500

    adjust to be more informative

--- DelimitedFileToolsTest/Program.cs
+++ DelimitedFileToolsTest/Program.cs
@@ -8,16 +8,25 @@ namespace DelimitedFileToolsTest
     {
         static void Main(string[] args)
         {
-            DelimitedFile file = new DelimitedFile(args[0]);
+            // initialize the reader, given the file to read from and that this file a header row
+            DelimitedFile file = new DelimitedFile(args[0], true);
 
+            // set delimiter values (these are the defaults)
+            file.NewlineCharacter = 10;
+            file.TextQualifierCharacter = 254;
+            file.CarriageReturnCharacter = 13;
+            file.ColumnDelimiterCharacter = 20;
+
+            // read in and handle an available line from the file
             while (file.ReadRow())
             {
+                // don't print out the header row
                 if (file.CurrentRowNumber == 1)
                 {
                     continue;
                 }
 
-                File.WriteAllText(string.Format(@"c:\users\ryan\desktop\foo_{0}.txt", file.CurrentRowNumber), file.GetColumn("text"));
+                Console.WriteLine(file.CurrentRow);
             }
         }
     }
